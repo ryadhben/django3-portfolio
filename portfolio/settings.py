4deac478b5aa671acb7c1589b5711740c7257465
Ryadh <ryadh199923@gmail.com>
2021-01-01 00:03:16 +0000

    Remove old stuff

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^1jz8+vc34$)+kwij@p!7i!a0y9%=!!dfk2)^hkxiu!qdesd5x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ryadhbenammour.pythonanywhere.com']
 
 
 # Application definition
@@ -120,5 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
